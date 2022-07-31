1764ad60c353b076c4686f67ef4341c9241756e3
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-31 19:47:41 +0600

    show all books

--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const BooksView = () => {
+
+  const books = useSelector((state)=> state.booksReducer.books);
+  console.log(books);
+  
   return (
     <div>
       <h2>List of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books && books.map((book)=> {
+            const {id, title, author} = book;
+            return <tr key={id}>
+              <td>{id} </td>
+              <td>{title} </td>
+              <td>{author} </td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
