d314cd36e1c1ffac52eb40c28e6018032c52998c
nazmul162001 <nazmulprime162001@gmail.com>
2022-08-04 21:57:30 +0600

    fixed commit

--- src/features/todos/AddTodo.js
+++ src/features/todos/AddTodo.js
@@ -4,7 +4,7 @@ const AddTodo = () => {
   return (
     <div>
       <h2>This is Todo Application using React Redux-toolkit</h2>
-      <h3>This is  another Todo Application using React Redux-toolkit</h2>
+      <h3>This is  another Todo Application using React Redux-toolkit</h3>
     </div>
   );
 };
